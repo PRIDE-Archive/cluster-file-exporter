0b19837c1808a82d4f9ceb2e126d0a5d391f2ebc
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-22 14:02:19 +0100

    extra checks on clustered psm report

--- src/main/java/uk/ac/ebi/pride/cluster/exporter/pipeline/pogo/visitors/PoGoEntryVisitorForClusteredPsmReport.java
+++ src/main/java/uk/ac/ebi/pride/cluster/exporter/pipeline/pogo/visitors/PoGoEntryVisitorForClusteredPsmReport.java
@@ -84,11 +84,16 @@ public class PoGoEntryVisitorForClusteredPsmReport implements PoGoEntryVisitor {
 
     @Override
     public PoGoEntry visit(PoGoEntry poGoEntry) {
-        poGoEntry.setExperiment(clusteredPSMReport.getClusterId().toString());
-        poGoEntry.setPeptide(translateSequence());
-        poGoEntry.setPsm(clusteredPSMReport.getClusterNumberPSMs());
-        // TODO - This parameter will be let as zero right now, in the future we need to work out a way to leave as it is right now, or compute its value
-        poGoEntry.setQuant(0d);
+        if (clusteredPSMReport != null) {
+            poGoEntry.setExperiment("---no_cluster_id_available---");
+            if (clusteredPSMReport.getClusterId() != null) {
+                poGoEntry.setExperiment(clusteredPSMReport.getClusterId().toString());
+            }
+            poGoEntry.setPeptide(translateSequence());
+            poGoEntry.setPsm(clusteredPSMReport.getClusterNumberPSMs());
+            // TODO - This parameter will be let as zero right now, in the future we need to work out a way to leave as it is right now, or compute its value
+            poGoEntry.setQuant(0d);
+        }
         return poGoEntry;
     }
 }
